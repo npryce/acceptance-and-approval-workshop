92d328f853603a160d7cab0f0062ecad0f810b6b
Nat Pryce <sw@natpryce.com>
2014-06-12 23:23:17 +0100

    python solution: refactor the test to get ready for testing projection

--- sample-solutions/python/test_pollution.py
+++ sample-solutions/python/test_pollution.py
@@ -12,13 +12,18 @@ def row_str(cells):
 def ident_to_str(s):
     return s.replace("_", " ")
 
-
-@approval(format="md")
-def test_parse_dataset(r):
-    r.write("Historical Air Pollution\n")
-    r.write("========================\n")
-    r.write("\n")
+def write_samples(r, samples, title=None):
+    if title is not None:
+        r.write(title + "\n")
+        r.write("="*len(title)+"\n")
+        r.write("\n")
     r.write(row_str(map(ident_to_str, p.sample._fields)))
     for row in p.load_history("../../datasets/air-quality.csv"):
         r.write(row_str(map(cell_str,row)))
+
+@approval(format="md")
+def test_parse_dataset(out):
+    write_samples(out, 
+                  samples=p.load_history("../../datasets/air-quality.csv"),
+                  title="Historical Air Pollution")
 
