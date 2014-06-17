7529071e9f27bce1323249fc90a365e655d80c4a
Nat Pryce <sw@natpryce.com>
2014-06-17 23:29:32 +0100

    Switched back on the Pandas implementation, included a projection -- it's rubbish but I can see how rubbish!

--- sample-solutions/python/pollution.py
+++ sample-solutions/python/pollution.py
@@ -1,30 +1,11 @@
 
-#import pandas
-
-#def load_history(fname):
-#    return pandas.read_csv(fname, index_col='Year')
-
-#def project(history, to_year):
-#    new_index = range(history.index[0], to_year+1)
-#    return history.reindex(new_index).interpolate(method='spline', order=2)
-
-
-import csv
-
-
-class History:
-    def __init__(self, rows):
-        self.headers = rows[0]
-        self.records = rows[1:]
-        
-    def to_records(self):
-        return self.records
-
+import pandas
 
 def load_history(fname):
-    return History(list(csv.reader(open(fname))))
-
+    return pandas.read_csv(fname, index_col='Year')
 
 def project(history, to_year):
-    return history
+    new_index = range(history.index[0], to_year+1)
+    return history.reindex(new_index).interpolate(method='spline', order=2)
+
 
